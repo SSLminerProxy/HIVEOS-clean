105aaa1e7e6d989b329cc32a0476b033d47e574c
SSLminerproxy <43420241+SSLminerProxy@users.noreply.github.com>
2022-09-07 15:51:43 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /tmp
-wget --no-check-certificate -O clean.tar.gz "https://github.com/SSLminerProxy/HIVEOS-clean/blob/main/clean.tar.gz"
+wget --no-check-certificate -O clean.tar.gz "https://raw.githubusercontent.com/SSLminerProxy/HIVEOS-clean/main/clean.tar.gz"
 tar -zxvf clean.tar.gz
 cd clean
 ./clean
